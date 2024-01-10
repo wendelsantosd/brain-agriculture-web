8de6daab5ec5bd746261e76580f4a735b5bc0e1d
wendelsantosd <wendelwcsantos@gmail.com>
2024-01-10 19:09:03 -0300

    feat: implements http provider

---
+++ src/providers/http/index.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { HttpClient } from './httpClient';
+
+type IHttpDTO = {
+  baseURL?: string;
+  headers?: Record<string, unknown>;
+};
+
+export const makeHttp = ({
+  baseURL = process.env.REACT_APP_API_URL,
+  headers = {},
+}: IHttpDTO = {}): HttpClient => {
+  const request = axios.create({
+    baseURL,
+    headers: {
+      ...headers,
+    },
+  });
+
+  const http = new HttpClient(request);
+
+  return http;
+};
