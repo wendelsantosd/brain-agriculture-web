1110dade81f76712e24b2edc3592d7ace73d8cba
wendelsantosd <wendelwcsantos@gmail.com>
2024-01-10 17:48:29 -0300

    chore: mock chart pizza

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,14 @@
-import { BoardValuesFarm } from "./components/BoardValuesFarm"
+import { BoardValuesFarm } from "./components/BoardValuesFarm/BoardValuesFarm"
+import { PizzasCharts } from "./components/PizzasCharts/PizzasCharts"
 
 export const Dashboard = (): React.ReactElement => {
   return <>
     <BoardValuesFarm />
+
+    <div className="mt-3 w-100 flex justify-between items-center">
+      <PizzasCharts />
+      <PizzasCharts />
+      <PizzasCharts />
+    </div>
   </>
 }
